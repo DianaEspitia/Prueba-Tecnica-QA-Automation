363897b7005d5efd81977bb6cc7f8b2d2385cff2
DianaEspitia <78936469+DianaEspitia@users.noreply.github.com>
2021-12-21 09:46:53 -0500

    Update prueba_tecnica_amazon.py

--- prueba_tecnica_amazon.py
+++ prueba_tecnica_amazon.py
@@ -29,40 +29,50 @@ barra.send_keys(Keys.ENTER)
 
 #Espera 
 wait = WebDriverWait(driver,10)
-elemento_consola = '/html/body/div[1]/div[2]/div[1]/div[1]/div/span[3]/div[2]/div[7]/div/div/div/div/div/div[1]/div/div[2]/div/span/a/div/img'
+bandera = '//*[@id="deliveryRefinements"]'
 
 try:
-    espera = wait.until(ec.visibility_of_element_located((By.XPATH, elemento_consola)))
+    espera = wait.until(ec.visibility_of_element_located((By.XPATH, bandera)))
 except Exception as e:
-    print("Fallas en la prueba, el elemento indicado no fue encontrado.")
+    print("Fallas en la prueba, el elemento indicado (bandera) no fue encontrado.")
     print()
     print(str(e))
     exit()
 
-#Elección de artículo                                     
-articulo = driver.find_element(By.XPATH, elemento_consola)
-if articulo is not None:
-    articulo.click()
-
-#Esperar a que se encuentre el botón para agregar el carrito de compras
+#Elección de artículo    
+#elemento_consola = '//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div/div/div/div[1]/div/div[2]/div/span/a/div/img'                                 
+elemento_consola = '/html/body/div[1]/div[2]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div/div/div/div[1]/div/div[2]/div/span/a/div/img'
 try:
-    boton_agregar_carrito = wait.until(ec.visibility_of_element_located((By.XPATH, '//*[@id="add-to-cart-button"]'))) 
+    #wait.until(ec.visibility_of_element_located((By.XPATH, elemento_consola)))
+    articulo = driver.find_element(By.XPATH, elemento_consola)
+    if articulo is not None:
+        articulo.click()
 except Exception as e2:
-    print("Fallas en la prueba. El botón para agregar el artículo al carrito de compras no se encontró, esto puede ser porque el producto seleccionado no se encuentra disponible.")
+    print("Fallas en la prueba, el articulo indicado no fue encontrado.")
     print()
     print(str(e2))
     exit()
+    
+#Esperar a que se encuentre el botón para agregar el carrito de compras
+try:
+    boton_agregar_carrito = wait.until(ec.visibility_of_element_located((By.XPATH, '//*[@id="add-to-cart-button"]'))) 
+except Exception as e3:
+    print("Fallas en la prueba. El botón para agregar el artículo al carrito de compras no se encontró, esto puede ser porque el producto seleccionado no se encuentra disponible.")
+    print()
+    print(str(e3))
+    exit()
 
 #Agregar artículo al carrito de compras
-agregar = driver.find_element_by_xpath('//*[@id="add-to-cart-button"]') 
+agregar = driver.find_element(By.XPATH, '//*[@id="add-to-cart-button"]') 
 if agregar is not None:
     agregar.click()
 
 #Ver carrito
-carrito = driver.find_element_by_xpath('//*[@id="nav-cart"]')
+carrito = driver.find_element(By.XPATH, '//*[@id="nav-cart"]')
 if carrito is not None:
     carrito.click()
 
 print("La prueba se ha completado con éxito")
 
 driver.close()
+
